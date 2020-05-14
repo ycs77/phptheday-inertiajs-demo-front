6e4663111ab96f12f98b7f29e781e964d3537410
Lucas Yang <yangchenshin77@gmail.com>
2020-05-14 20:23:05 +0800

    Optimize todolist

--- src/Pages/Todolist.vue
+++ src/Pages/Todolist.vue
@@ -2,17 +2,6 @@
   <div class="max-w-md pt-6 mx-auto">
     <h1 class="text-purple-700 text-3xl text-center">待辦事項</h1>
     <div class="bg-white mt-4 border border-purple-200 divide-y divide-purple-200 rounded-md shadow-md">
-      <label v-for="todo in todos" :key="todo.id" class="flex px-6 py-4 cursor-pointer select-none">
-        <div class="mr-4">
-          <input type="checkbox" class="form-checkbox" v-model="todo.checked" @change="onCheckTodo(todo.id, todo.checked)">
-        </div>
-        <div class="flex-1" :class="todo.checked ? 'text-gray-500 line-through' : ''">{{ todo.content }}</div>
-        <button class="px-2 text-red-600 hover:text-red-400 focus:outline-none" @click="removeTodo(todo.id)">
-          <svg class="fill-current h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M8 6V4c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2h5a1 1 0 0 1 0 2h-1v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V8H3a1 1 0 1 1 0-2h5zM6 8v12h12V8H6zm8-2V4h-4v2h4zm-4 4a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1z"/>
-          </svg>
-        </button>
-      </label>
       <form class="flex px-6 py-4" @submit.prevent="createTodo">
         <div class="flex-1 mr-4">
           <input type="text" class="form-input w-full" placeholder="請輸入待辦事項..." v-model="form.content">
@@ -24,6 +13,19 @@
           <button class="px-4 py-2 text-white bg-purple-500 rounded hover:bg-purple-400 focus:outline-none">新增</button>
         </div>
       </form>
+      <div v-for="todo in todos" :key="todo.id" class="flex select-none">
+        <label class="flex flex-1 px-6 py-4 cursor-pointer">
+          <div class="mr-4">
+            <input type="checkbox" class="form-checkbox" v-model="todo.checked" @change="onCheckTodo(todo.id, todo.checked)">
+          </div>
+          <div class="flex-1" :class="todo.checked ? 'text-gray-500 line-through' : ''">{{ todo.content }}</div>
+        </label>
+        <button class="px-6 text-red-600 hover:text-red-400 focus:outline-none" @click="removeTodo(todo.id)">
+          <svg class="fill-current h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M8 6V4c0-1.1.9-2 2-2h4a2 2 0 0 1 2 2v2h5a1 1 0 0 1 0 2h-1v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V8H3a1 1 0 1 1 0-2h5zM6 8v12h12V8H6zm8-2V4h-4v2h4zm-4 4a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1zm4 0a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0v-6a1 1 0 0 1 1-1z"/>
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -34,12 +36,12 @@ export default {
     todos: [
       {
         id: 1,
-        content: '開發 A 功能',
+        content: '修復 A bug',
         checked: false
       },
       {
         id: 2,
-        content: '修復 B Bug',
+        content: '開發 B 功能',
         checked: true
       }
     ],
