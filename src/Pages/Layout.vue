091b03f8a03844e50069e3cd470aa92fe7a7390d
Lucas Yang <yangchenshin77@gmail.com>
2020-05-14 16:53:15 +0800

    Update layout

--- src/Pages/Layout.vue
+++ src/Pages/Layout.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
     <nav class="bg-white shadow">
-      <div class="container px-4 flex items-center py-4 mx-auto">
+      <div class="container p-4 mx-auto flex items-center">
         <div class="text-xl font-semibold text-purple-600">Inertia 範例</div>
         <ul class="flex space-x-4 ml-6">
-          <li><a class="text-purple-400 uppercase hover:text-purple-600" href="/">Home</a></li>
-          <li><a class="text-purple-400 uppercase hover:text-purple-600" href="/about">About</a></li>
-          <li><a class="text-purple-400 uppercase hover:text-purple-600" href="/posts">Posts</a></li>
+          <li><a class="inline-block text-purple-300 hover:text-purple-600" href="/">Home</a></li>
+          <li><a class="inline-block text-purple-300 hover:text-purple-600" href="/about">About</a></li>
+          <li><a class="inline-block text-purple-300 hover:text-purple-600" href="/posts">Posts</a></li>
         </ul>
       </div>
     </nav>
 
-    <div class="container px-4 mx-auto py-4 text-center text-3xl text-purple-800">
+    <div class="container p-4 mx-auto text-center text-3xl text-purple-800">
       <slot></slot>
     </div>
   </div>
