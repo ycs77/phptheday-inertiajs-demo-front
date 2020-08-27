1dbf4ccd27209fac04d6ddf493ed40fa9684a98a
Lucas Yang <yangchenshin77@gmail.com>
2020-08-27 17:08:42 +0800

    Remove postcss config

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,6 @@ const mix = require('laravel-mix')
 
 mix.postCss('src/app.css', 'src/tailwindcss.css', [
   require('postcss-import'),
-  require('tailwindcss')
+  require('tailwindcss'),
+  require('autoprefixer')
 ])
